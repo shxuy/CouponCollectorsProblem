ed86b1bf2a2702f12ba80e4ed3a1c574d9d14582
Haitian Yang <haitianyang@outlook.com>
2019-12-07 21:10:05 +0800

    update coupon_collector_solver.py

--- coupon_collector_solver.py
+++ coupon_collector_solver.py
@@ -22,8 +22,9 @@ class CouponCollectorSolver(object):
         if n < 0:
             raise ValueError('the parameter n should be a natural number')
         self.__n = n
-        self.__max_pdf_cdf_t = int(self.expectation * 2)  # 概率密度函数和累计分布函数的自变量最大值，两倍于数学期望应该够了
+        self.__max_pdf_cdf_t = int(self.expectation * 3)  # 概率密度函数和累计分布函数的自变量最大值，三倍于数学期望应该够了
         self.__table = Stirling2LookupTable(self.__max_pdf_cdf_t, self.__n)
+        self.__cdf_series = None
 
     @property
     def n(self):
@@ -39,6 +40,14 @@ class CouponCollectorSolver(object):
         for i in range(1, self.__n + 1):
             sum += self.__n / i
         return sum
+
+    @property
+    def max_pdf_cdf_t(self):
+        """
+        返回概率密度函数和累计分布函数的自变量最大值
+        :return: 概率密度函数和累计分布函数的自变量最大值: int
+        """
+        return self.__max_pdf_cdf_t
 
     @property
     def variance(self):
@@ -76,3 +85,98 @@ class CouponCollectorSolver(object):
         if t < 0 or t > self.__max_pdf_cdf_t:
             raise ValueError('the parameter t should be >= 0 and <= %d' % self.__max_pdf_cdf_t)
         return math.factorial(self.__n) * self.__table.stirling2(t, self.__n) / pow(self.__n, t)
+
+    def two_sided_confidence_interval(self, confidence_level: float):
+        """
+        双侧置信区间，即置信区间为(lower_bound, upper_bound)
+        :param confidence_level: 置信水平
+        :return: 双侧置信区间，形式为一个包括两个int的list
+        """
+        if not isinstance(confidence_level, float):
+            raise TypeError('the parameter confidence_level should be a float number')
+        if confidence_level <= 0 or confidence_level >= 1:
+            raise ValueError('the parameter confidence_level should be > 0 and < 1')
+        alpha = 1 - confidence_level
+        self.__calculate_all_cdf()
+        lower_bound = self.__binary_search_largest_value(self.__cdf_series, alpha / 2)
+        upper_bound = self.__binary_search_smallest_value(self.__cdf_series, 1 - alpha / 2)
+        return [lower_bound, upper_bound]
+
+    def one_sided_lower_confidence_limit(self, confidence_level: float):
+        """
+        单侧置信下限，即置信区间为(lower_bound, math.inf)
+        :param confidence_level: 置信水平
+        :return: 单侧置信下限lower_bound: int
+        """
+        if not isinstance(confidence_level, float):
+            raise TypeError('the parameter confidence_level should be a float number')
+        if confidence_level <= 0 or confidence_level >= 1:
+            raise ValueError('the parameter confidence_level should be > 0 and < 1')
+        self.__calculate_all_cdf()
+        alpha = 1 - confidence_level
+        lower_bound = self.__binary_search_largest_value(self.__cdf_series, alpha)
+        return lower_bound
+
+    def one_sided_upper_confidence_limit(self, confidence_level: float):
+        """
+        单侧置信上限，即置信区间为(0, upper_bound)
+        :param confidence_level: 置信水平
+        :return: 单侧置信上限upper_bound: int
+        """
+        if not isinstance(confidence_level, float):
+            raise TypeError('the parameter confidence_level should be a float number')
+        if confidence_level <= 0 or confidence_level >= 1:
+            raise ValueError('the parameter confidence_level should be > 0 and < 1')
+        self.__calculate_all_cdf()
+        upper_bound = self.__binary_search_smallest_value(self.__cdf_series, confidence_level)
+        return upper_bound
+
+    def __calculate_all_cdf(self):
+        """
+        计算区间[0, self.__max_pdf_cdf_t]上的所有累计分布函数值
+        :return: 无
+        """
+        if not self.__cdf_series:
+            self.__cdf_series = [self.cdf(x) for x in range(self.__max_pdf_cdf_t + 1)]
+
+    @staticmethod
+    def __binary_search_largest_value(array: list, lookup_value: float):
+        """
+        二分查找。查找小于或等于 lookup_value 的最大值对应的序号
+        :param array: 一个升序序列
+        :param lookup_value: 查找的值
+        :return: 序号
+        """
+        low = 0
+        high = len(array) - 1
+        while low <= high:
+            middle = low + (high - low) // 2
+            middle_value = array[middle]
+            if lookup_value > middle_value:
+                low = middle + 1
+            elif lookup_value < middle_value:
+                high = middle - 1
+            else:
+                return middle
+        return high
+
+    @staticmethod
+    def __binary_search_smallest_value(array: list, lookup_value: float):
+        """
+        二分查找。查找大于或等于 lookup_value 的最小值对应的序号
+        :param array: 一个升序序列
+        :param lookup_value: 查找的值
+        :return: 序号
+        """
+        low = 0
+        high = len(array) - 1
+        while low <= high:
+            middle = low + (high - low) // 2
+            middle_value = array[middle]
+            if lookup_value > middle_value:
+                low = middle + 1
+            elif lookup_value < middle_value:
+                high = middle - 1
+            else:
+                return middle
+        return low
--- main.py
+++ main.py
@@ -5,43 +5,15 @@
 
 
 """
-卡片收集问题: https://blog.csdn.net/wenrr89/article/details/54688469
-python实现第二类Stirling数: https://blog.csdn.net/LuYi_WeiLin/article/details/85390096
-计算排列组合数-python: https://blog.csdn.net/weixin_41481113/article/details/82845269
-如果一个男/女生说，他/她集齐了十二个星座的前任: https://www.zhihu.com/question/38331955
-
-
+赠券收集问题
 """
 
-import math
-from stirling2_lookup_table import Stirling2LookupTable
 from coupon_collector_solver import CouponCollectorSolver
 
-def stirling2(n: int, m: int):
-    if m > n or m == 0:
-        return 0
-    if n == m:
-        return 1
-    if m == 1:
-        return 1
-    return stirling2(n - 1, m - 1) + stirling2(n - 1, m) * m
 
-
-
-def cdf(n: int, m: int):
-    return math.factorial(m) * stirling2(n, m) / pow(m, n)
-
-def pdf(n: int, m: int):
-    return math.factorial(m) * stirling2(n - 1, m - 1) / pow(m, n)
-
-# print(stirling2(10, 9))
-stirling2_table = Stirling2LookupTable(9, 9)
-#print(stirling2_table)
-solver = CouponCollectorSolver(0)
-
-for i in range(0, 10):
-    print(solver.pdf(i))
-
-
-def cdf(number_of_cards: int):
-    pass
+solver = CouponCollectorSolver(12)
+for i in range(110):
+    print(str(i) + " " + str(solver.cdf(i)))
+print(solver.one_sided_lower_confidence_limit(0.95))
+print(solver.one_sided_upper_confidence_limit(0.95))
+print(solver.two_sided_confidence_interval(0.90))
