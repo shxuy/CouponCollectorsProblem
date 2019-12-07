92902253a3be889ebabc99b2c21c2340643a323c
Haitian Yang <haitianyang@outlook.com>
2019-12-07 19:28:15 +0800

    add stirling2_lookup_table.py

--- main.py
+++ main.py
@@ -9,14 +9,13 @@
 python实现第二类Stirling数: https://blog.csdn.net/LuYi_WeiLin/article/details/85390096
 计算排列组合数-python: https://blog.csdn.net/weixin_41481113/article/details/82845269
 如果一个男/女生说，他/她集齐了十二个星座的前任: https://www.zhihu.com/question/38331955
-赠券问题的均值和方差：https://brilliant.org/wiki/coupon-collector-problem/
-           n
-Var[X]=n^2 ∑ (1/k^2) - n * Hn
-          k=1
+
 
 """
 
 import numpy as np
+import math
+from stirling2_lookup_table import Stirling2LookupTable
 
 def stirling2(n: int, m: int):
     if m > n or m == 0:
@@ -27,40 +26,22 @@ def stirling2(n: int, m: int):
         return 1
     return stirling2(n - 1, m - 1) + stirling2(n - 1, m) * m
 
-"""
+
 for i in range(10):
     s = ""
     for j in range(10):
         s += str(stirling2(i, j)) + "\t"
-    #print(s)
-"""
-
-
-def factorial(n: int):
-    """
-    高精度阶乘
-    scipy.special里的factorial返回一个浮点数，容易导致计算结果不精准，所以这里重复造轮子
-    :param n: 自然数
-    :return: n!
-    """
-    if not isinstance(n, int) or n < 0:
-        raise Exception('阶乘的参数应为正整数')
-    if n == 0 or n == 1:
-        return 1
-    product = 1
-    for i in range(1, n + 1):
-        product *= i
-    return product
-
+    print(s)
 
 def cdf(n: int, m: int):
-    return factorial(m) * stirling2(n, m) / pow(m, n)
+    return math.factorial(m) * stirling2(n, m) / pow(m, n)
 
 def pdf(n: int, m: int):
-    return factorial(m) * stirling2(n - 1, m - 1) / pow(m, n)
+    return math.factorial(m) * stirling2(n - 1, m - 1) / pow(m, n)
 
-print(stirling2(10, 9))
-
+# print(stirling2(10, 9))
+stirling2_table = Stirling2LookupTable(9, 9)
+print(stirling2_table)
 
 def cdf(number_of_cards: int):
     pass
---
+++ stirling2_lookup_table.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# @author Nick
+
+"""
+一口气算完 (n + 1) * (m + 1) 个第二个Stirling数，然后查阅使用，避免重复计算
+"""
+
+
+class Stirling2LookupTable(object):
+
+    def __init__(self, n_max: int, m_max: int):
+        if not isinstance(n_max, int):
+            raise TypeError('the parameter n_max should be a natural number')
+        if n_max < 0:
+            raise ValueError('the parameter n_max should be a natural number')
+        if not isinstance(m_max, int):
+            raise TypeError('the parameter m_max should be a natural number')
+        if m_max < 0:
+            raise ValueError('the parameter m_max should be a natural number')
+        self.__n_max = n_max
+        self.__m_max = m_max
+        # 我一开始使用 np.zeros((n_max + 1, m_max + 1), dtype=int) 来初始化 self.__table，可惜当 n_max 和 m_max 足够大
+        # （比如 n_max 和 m_max 分别为30）时，表中的数字会溢出。
+        self.__table = [[0 for x in range(m_max + 1)] for y in range(n_max + 1)]
+        self.__table[0][0] = 1
+        if self.__m_max >= 1:
+            for i in range(1, self.__n_max + 1):
+                self.__table[i][1] = 1
+        for i in range(2, self.__m_max + 1):
+            for j in range(i, self.__n_max + 1):
+                self.__table[j][i] = self.__table[j - 1][i - 1] + self.__table[j - 1][i] * i
+
+    @property
+    def n_max(self):
+        return self.__n_max
+
+    @property
+    def m_max(self):
+        return self.__m_max
+
+    def stirling2(self, n: int, m: int):
+        if not isinstance(n, int):
+            raise TypeError('the parameter n should be an integer')
+        if n < 0 or n > self.__n_max:
+            raise ValueError('the parameter n should be >= 0 and <= %d' % self.__n_max)
+        if not isinstance(m, int):
+            raise TypeError('the parameter m should be an integer')
+        if m < 0 or m > self.__m_max:
+            raise ValueError('the parameter m should be >= 0 and <= %d' % self.__m_max)
+        return self.__table[n][m]
+
+    def __str__(self):
+        return '[' + ',\n'.join([str(x) for x in self.__table]) + ']'
