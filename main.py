95596af658633368133178b8e1f8f6f2d468cb76
Haitian Yang <haitianyang@outlook.com>
2019-12-08 15:58:52 +0800

    add figures of probability density function and cumulative distribution function

--- main.py
+++ main.py
@@ -9,150 +9,9 @@
 
 __author__ = 'Nick Yang'
 
-'''
-import sys
-import traceback
-import tkinter
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib.backend_bases import key_press_handler
-from matplotlib.figure import Figure
-from matplotlib import pyplot as plt
-from coupon_collector_solver import CouponCollectorSolver
-
-
-def pdf_and_cdf(solver):
-    """
-    根据一个赠券收集问题解算实例，绘制概率密度函数和累积分布函数
-    :param solver: 赠券收集问题解算实例
-    :return: 一个包含概率密度函数和累积分布函数的matplotlib.figure
-    """
-    figure = Figure(figsize=(8, 4))
-    # 绘制概率密度函数
-    pdf = figure.add_subplot(121)
-    pdf.plot(solver.horizontal_axis, solver.all_pdf, '.b')
-    plt.title('probability density function')
-    plt.xlabel('times of selections')
-    plt.ylabel('probability')
-    plt.xlim(0, solver.max_pdf_cdf_t)
-    plt.ylim(0)
-    # 绘制累积分布函数
-    cdf = figure.add_subplot(122)
-    cdf.plot(solver.horizontal_axis, solver.all_cdf, '.b')
-    plt.title('cumulative distribution function')
-    plt.xlabel('times of selections')
-    plt.ylabel('probability')
-    plt.xlim(0, solver.max_pdf_cdf_t)
-    plt.ylim(0, 1)
-    return figure
-
-
-def main(argv):
-    solver = CouponCollectorSolver(12)
-    figure = pdf_and_cdf(solver)
-    figure.show()
-    print(solver.one_sided_lower_confidence_limit(0.95))
-    print(solver.one_sided_upper_confidence_limit(0.95))
-    print(solver.two_sided_confidence_interval(0.90))
-
-
-if __name__ == '__main__':
-    try:
-        exit(main(sys.argv[1:]))
-    except TypeError or ValueError or Exception:
-        traceback.print_exc()
-
-
-
-
-root = tkinter.Tk()
-root.wm_title("coupon collector's problem")
-
-# 画布的大小和分别率
-fig = Figure(figsize=(10, 4))
-
-# 利用子图画图
-axc = fig.add_subplot(121)
-axc.plot([1, 2, 3, 4, 5], "ob")
-
-# 创建画布控件
-canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-canvas.draw()
-# 显示画布控件
-canvas.get_tk_widget().pack()
-
-# 创建工具条控件
-toolbar = NavigationToolbar2Tk(canvas, root)
-toolbar.update()
-# 显示工具条控件
-#canvas.get_tk_widget().pack()
-
-
-# 绑定快捷键函数
-def on_key_press(event):
-    print("you pressed {}".format(event.key))
-    key_press_handler(event, canvas, toolbar)
-
-
-# 调用快捷键函数
-canvas.mpl_connect("key_press_event", on_key_press)
-
-
-# 退出函数
-def _quit():
-    root.quit()
-    root.destroy()
-
-
-# 消息循环
-tkinter.mainloop()
-'''
-'''
-import numpy as np
-import tkinter
+import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-from coupon_collector_solver import CouponCollectorSolver
-
-
-def drawPic():
-    fig.clf()
-    # 绘制概率密度函数
-    pdf = fig.add_subplot(121)
-    pdf.scatter(solver.horizontal_axis, solver.all_pdf, s=1)
-    pdf.set_title('probability density function')
-    pdf.set_xlabel('times of selections')
-    pdf.set_ylabel('probability')
-    pdf.set_xlim(0, solver.max_pdf_cdf_t)
-    pdf.set_ylim(0)
-    # 绘制累积分布函数
-    cdf = fig.add_subplot(122)
-    cdf.scatter(solver.horizontal_axis, solver.all_cdf, s=1)
-    cdf.set_title('cumulative distribution function')
-    cdf.set_xlabel('times of selections')
-    cdf.set_ylabel('probability')
-    cdf.set_xlim(0, solver.max_pdf_cdf_t)
-    cdf.set_ylim(0, 1)
-    fig_canvas.draw()
-
-
-
-if __name__ == '__main__':
-    solver = CouponCollectorSolver(12)
-    root = tkinter.Tk()
-    tkinter.Label(root, text='number of types: ').grid(row=0, column=0)
-    inputEntry = tkinter.Entry(root)
-    inputEntry.grid(row=0, column=1)
-    inputEntry.insert(0, '12')
-    tkinter.Button(root, text='draw', command=drawPic).grid(row=0, column=2, columnspan=3)
-    fig = Figure(figsize=(8, 4), dpi=100)
-    fig_canvas = FigureCanvasTkAgg(fig, master=root)
-    fig_canvas.get_tk_widget().grid(row=1, columnspan=3)
-
-    #启动事件循环
-    root.mainloop()
-    
-'''
-import tkinter as tk
 from coupon_collector_solver import CouponCollectorSolver
 
 
@@ -166,7 +25,7 @@ class Application(tk.Frame):
         self.master.title("Coupon Collector's problem")
         self.master.iconbitmap('coupon.ico')  # 经反复实验，只有windows平台能改图标
         self.pack()
-        self.center_window(500, 500)
+        self.center_window(800, 600)
         # 最上面空一行
         self.frame0 = tk.Frame(self.master)
         self.frame0.pack(side=tk.TOP, ipady=10)
@@ -230,6 +89,13 @@ class Application(tk.Frame):
         self.label_annotation_one_sided_confidence_interval.pack(side=tk.LEFT)
         self.label_one_sided_confidence_interval = tk.Label(self.frame7, width=30)
         self.label_one_sided_confidence_interval.pack(side=tk.LEFT)
+        # 概率密度函数和累积分布函数绘制区
+        self.frame8 = tk.Frame(self.master)
+        self.frame8.pack(side=tk.TOP, pady=3)
+        self.pdf_and_cdf = Figure(figsize=(8, 4), dpi=100)
+        self.canvas = FigureCanvasTkAgg(self.pdf_and_cdf, master=self.frame8)
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM)
+        self.canvas.draw()
 
     def on_scale_n_change(self, n: int):
         self.n = int(n)
@@ -238,6 +104,26 @@ class Application(tk.Frame):
         self.label_expectation['text'] = str(self.solver.expectation)
         self.label_variance['text'] = str(self.solver.variance)
         self.update_interval()
+        # 清空画布
+        self.pdf_and_cdf.clf()
+        # 准备绘制概率密度函数
+        pdf = self.pdf_and_cdf.add_subplot(121)
+        pdf.scatter(self.solver.horizontal_axis, self.solver.all_pdf, s=1)
+        pdf.set_title('probability density function')
+        pdf.set_xlabel('times of selections')
+        pdf.set_ylabel('probability')
+        pdf.set_xlim(0, self.solver.max_pdf_cdf_t)
+        pdf.set_ylim(0)
+        # 准备绘制累积分布函数
+        cdf = self.pdf_and_cdf.add_subplot(122)
+        cdf.scatter(self.solver.horizontal_axis, self.solver.all_cdf, s=1)
+        cdf.set_title('cumulative distribution function')
+        cdf.set_xlabel('times of selections')
+        cdf.set_ylabel('probability')
+        cdf.set_xlim(0, self.solver.max_pdf_cdf_t)
+        cdf.set_ylim(0, 1)
+        # 真正画上去
+        self.canvas.draw()
 
     def on_scale_confidence_level_change(self, confidence_level: float):
         self.confidence_level = float(confidence_level)
@@ -250,7 +136,7 @@ class Application(tk.Frame):
             self.label_two_sided_confidence_interval['text'] = '(%d, %d)' % (interval[0], interval[1])
             self.label_one_sided_confidence_interval['text'] = \
                 '(0, %d) or (%d, math.inf)' % (self.solver.one_sided_upper_confidence_limit(self.confidence_level),
-                                                self.solver.one_sided_lower_confidence_limit(self.confidence_level))
+                                               self.solver.one_sided_lower_confidence_limit(self.confidence_level))
 
     def center_window(self, w: int, h: int):
         """
